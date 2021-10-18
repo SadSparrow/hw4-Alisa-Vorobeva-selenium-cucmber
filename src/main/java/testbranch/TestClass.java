ea92cd58ec9ad9b8e44a40cccac9913f45b5ea73
Alisa <ulmoniss@yandex.ru>
2021-10-18 10:46:33 +0300

    test another commit_1
    test commit 3_2
    
    test commit 4_3

---
+++ src/main/java/testbranch/TestClass.java
@@ -0,0 +1,11 @@
+package testbranch;
+
+public class TestClass {
+    //test squash
+    public static void main(String[] args) {
+        String s = "new string 123";
+        System.out.println(s.substring(0, s.length() - 2));
+        System.out.println(s.substring(4));
+        System.out.println(s.substring(3));
+    }
+}
