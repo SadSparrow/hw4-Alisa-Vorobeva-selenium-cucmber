fc187592bf797e0d32919306a5bf445b1b80e9ae
Alisa <ulmoniss@yandex.ru>
2021-10-18 11:29:11 +0300

    main commit message
    test commit 1.2

--- src/main/java/testbranch/TestSquash.java
+++ src/main/java/testbranch/TestSquash.java
@@ -1,12 +1,9 @@
 package testbranch;
 
-public class TestClass {
+public class TestSquash {
     //test squash
     public static void main(String[] args) {
         String s = "new string 123";
         System.out.println(s.substring(0, s.length() - 2));
-        System.out.println(s.substring(4));
-        System.out.println(s.substring(3));
-        System.out.println(s.substring(3));
     }
 }
