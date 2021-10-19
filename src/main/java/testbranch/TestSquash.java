793d923a81b3547bceea3aaa230f11b413af0e71
Alisa <ulmoniss@yandex.ru>
2021-10-19 14:46:37 +0300

    delete

--- src/main/java/testbranch/TestSquash.java
+++
@@ -1,9 +0,0 @@
-package testbranch;
-
-public class TestSquash {
-    //test squash
-    public static void main(String[] args) {
-        String s = "new string 123";
-        System.out.println(s.substring(0, s.length() - 2));
-    }
-}
