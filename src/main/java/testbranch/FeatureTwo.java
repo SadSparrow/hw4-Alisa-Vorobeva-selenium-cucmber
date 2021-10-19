017aa7350df854d9cf1d82e4137687e65d5dda70
Alisa <ulmoniss@yandex.ru>
2021-10-19 14:51:33 +0300

    feature 2

---
+++ src/main/java/testbranch/FeatureTwo.java
@@ -0,0 +1,11 @@
+package testbranch;
+
+public class FeatureTwo {
+
+    public static void main(String[] args) {
+        String s = "Feature 2!!!";
+        System.out.println(s);
+        System.out.println(s + "FIX");
+        System.out.println(s + "NEW FIX");
+    }
+}
