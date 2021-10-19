769a5aec289f230e2487a6667d4d45b219cb9819
Alisa <ulmoniss@yandex.ru>
2021-10-19 10:45:15 +0300

    test commit 2.1

---
+++ src/main/java/testbranch/Test.java
@@ -0,0 +1,14 @@
+package testbranch;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String s = "new string 123";
+        System.out.println(s.substring(0, s.length() - 2));
+        System.out.println(s.substring(3));
+        System.out.println(s.substring(2));
+        System.out.println(s.substring(4));
+        System.out.println(s.substring(5));
+        System.out.println(s.substring(5));
+    }
+}
